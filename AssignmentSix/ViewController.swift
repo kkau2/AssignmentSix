d7e3d1e109987a70799e2a3d8ddca69d2ff0047d
kkau2 <46541270+kkau2@users.noreply.github.com>
2019-03-01 15:50:19 -1000

    Completed Assignment #6

--- AssignmentSix/ViewController.swift
+++ AssignmentSix/ViewController.swift
@@ -22,6 +22,7 @@ class ViewController: UIViewController {
         
         //1. Set the displayTextLabel text to a string with no characters
         
+        self.displayTextLabel.text = ""
         
     }
 
@@ -30,14 +31,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    
+  //Create the IBAction
     @IBAction func setLabelButtonPressed(_ sender: Any) {
     //2. Create a local variable of type String to hold the user input text
+        var userText:String!
     
     //3. Set userText to the current text in the text field or user input text
+        userText = self.textField.text
     
     //4. Set the displayTextLabel text to the user input text
-    
+    self.displayTextLabel.text = userText
     
     }
     
